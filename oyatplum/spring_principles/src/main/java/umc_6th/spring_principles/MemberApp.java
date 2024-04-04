5d823fbb505e0eb02e8f135c51e3688ff7e60332
이예지 <oyatpeach@naver.com>
2024-04-04 17:07:58 +0900

    Feat : 회원 도메인 개발 및 테스트 생성

---
+++ oyatplum/spring_principles/src/main/java/umc_6th/spring_principles/MemberApp.java
@@ -0,0 +1,18 @@
+package umc_6th.spring_principles;
+
+import umc_6th.spring_principles.member.Grade;
+import umc_6th.spring_principles.member.Member;
+import umc_6th.spring_principles.member.MemberService;
+import umc_6th.spring_principles.member.MemberServiceImpl;
+
+public class MemberApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new member = " + member.getName());
+        System.out.println("find Member = " + findMember.getName());
+    }
+}
