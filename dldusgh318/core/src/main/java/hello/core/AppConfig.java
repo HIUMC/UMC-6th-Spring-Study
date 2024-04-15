e2b0809b38e86eb6d592d42cf5121c95721fd377
dldusgh318 <pololydotoly@gmail.com>
2024-04-15 18:38:04 +0900

    feat: 3주차 스프링핵심원리 (2/3)

--- dldusgh318/core/src/main/java/hello/core/AppConfig.java
+++ dldusgh318/core/src/main/java/hello/core/AppConfig.java
@@ -14,22 +14,41 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
+    //@Bean memberService -> new MemoryMemberRepository()
+    //@Bean orderService -> new MemoryMemberRepository()
+    //무상태 아니지않음? ㅜㅜ
+
+    //call AppConfig.memberService
+    //call AppConfig.memberRepository
+    //call AppConfig.memberRepository
+    //call AppConfig.orderService
+    //call AppConfig.memberRepository
+
+    //call AppConfig.memberService
+    //call AppConfig.memberRepository
+    //call AppConfig.orderService
+
+
     @Bean   //Bean덕에 매소드가 springcontainer에 저장됨
     public MemberService memberService(){
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
     @Bean
     public MemoryMemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService(){
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(),discountPolicy());
     }
 
     @Bean
     public DiscountPolicy discountPolicy(){
+
         return new RateDiscountPolicy();
     }
 }
