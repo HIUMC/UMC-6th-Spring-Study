5851a5bc48702e1869c78c64a05af7ba3d38d572
dldusgh318 <pololydotoly@gmail.com>
2024-05-12 21:15:21 +0900

    섹션 6까지

--- dldusgh318/jpashop (1)/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ dldusgh318/jpashop (1)/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,8 +1,13 @@
 package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.*;
+import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Inheritance(strategy =InheritanceType.SINGLE_TABLE)
@@ -17,4 +22,22 @@ public abstract class Item {
     private String name;
     private int price;
     private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories=new ArrayList<Category>();
+
+    //비즈니스 로직//
+    // 재고 수량을 증가하는 로직
+    public void addStock(int quantity){
+        this.stockQuantity+=quantity;
+    }
+
+    //재고 수량을 감소하는 로직
+    public void removeStock(int quantity){
+        int restStock=this.stockQuantity-quantity;
+        if(restStock<0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity=restStock;
+    }
 }
